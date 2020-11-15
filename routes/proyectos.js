7c60a308aa5719eb194f3488e672483682584c86
HecAllende <54869462+HectorAllende@users.noreply.github.com>
2020-11-14 22:45:45 -0300

    MERNTasks

---
+++ routes/proyectos.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const router = express.Router();
+const proyectoController = require('../controllers/proyectoController')
+const auth = require('../middleware/auth')
+const {check} = require('express-validator')
+
+// Crea proyectos
+// api/proyectos
+
+router.post('/', 
+    auth,
+    [
+        check('nombre', 'El nombre del proyecto es obligatorio').not().isEmpty()
+    ],
+
+    proyectoController.crearProyecto
+
+)
+//obtener todos los proyectos
+router.get('/', 
+    auth,
+    proyectoController.obtenerProyectos
+
+)
+
+
+// Actualizar proyecto via ID
+router.put('/:id', 
+    auth,
+    [
+        check('nombre', 'El nombre del proyecto es obligatoio').not().isEmpty()
+    ],
+    proyectoController.actualizarProyecto
+);
+
+// ELiminar un proyecto
+router.delete('/:id', 
+    auth,
+     proyectoController.eliminarProyecto
+);
+
+
+
+module.exports = router;
